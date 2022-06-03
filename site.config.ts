e92f11b9830c9b4656e6f4ea843d1c369fa3d02e
huo-shu <58618251+huo-shu@users.noreply.github.com>
2022-06-03 17:17:17 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'cc41a5063b0e412e848a0bae423547ee',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://cnotion.notion.site/cc41a5063b0e412e848a0bae423547ee',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
